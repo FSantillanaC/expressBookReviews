c195c84697a758f9cbba35f66d45dfb1db880681
FSantillanaC <fer.santillana@kyndryl.com>
2025-04-19 01:28:24 -0400

    message

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -38,9 +38,36 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    const username = req.body.username;
+    const review = req.body.review;
+    if(!isbn||!username||!review){
+      return res.status(400).json({message: "Error: Invalid request!"});
+    }
+    if(!isValid(username)){
+      return res.status(400).json({message: "Error: Invalid username!"});
+    }
+    if(!books[isbn]){
+      return res.status(400).json({message: "Error: Invalid ISBN!"});
+    }
+    books[isbn].reviews[username]=review;
+    return res.status(200).json({message: "Review added successfully!"});
+  });
+
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.body.username;
+    if(!isbn||!username){
+      return res.status(400).json({message: "Error: Invalid request!"});
+    }
+    if(!isValid(username)){
+      return res.status(400).json({message: "Error: Invalid username!"});   
+    }
+    if(!books[isbn]){
+      return res.status(400).json({message: "Error: Invalid ISBN!"});
+    }
+    delete books[isbn].reviews[username];
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
